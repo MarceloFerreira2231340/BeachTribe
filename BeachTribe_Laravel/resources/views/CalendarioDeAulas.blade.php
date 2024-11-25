57fe0c1bf452141ae77235a7c1fa1d61ea3ed4c9
JoaoBonita2430 <2223235@my.ipleiria.pt>
2024-11-25 20:51:17 +0000

    BeachTribe_Laravel_V1.0
    Laravel

---
+++ BeachTribe_Laravel/resources/views/CalendarioDeAulas.blade.php
@@ -0,0 +1,173 @@
+@extends('layout.master')
+@section('title', 'Calendário de Aulas')
+
+ @section('styles')
+ <link rel="stylesheet" href="{{asset('css/normalize.css')}}">
+ <link rel="stylesheet" href="{{asset('css/Joao/stylesjb.css')}}">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+@endsection
+
+@section('content')
+    <div class="intro">
+        <h1><b>Calendário de Aulas</b></h1>
+        <h3>Aqui podes consultar as aulas e os horários disponíveis durante por mês</h3>
+    </div>
+    <div class="fix">
+    <div class="tltmes">
+        <h2>Maio, 2025</h2>
+    </div>
+
+    <div class="border rounded shadow-sm cardaula">
+
+        <div class="areadate">
+            <h2>SAB</h2>
+            <h1>17</h1>
+        </div>
+
+        <div class="vr"></div>
+
+        <div class="horaloc">
+            <div class="hora">
+                <i class="bi bi-clock-fill"></i>
+                <p>9:00 - 11:00</p>
+            </div>
+            <div class="loc">
+                <i class="bi bi-geo-alt-fill"></i>
+                <p>Praia S. Pedro de Moel</p>
+            </div>
+        </div>
+
+        <div class="tipoprice">
+            <div class="tipo">
+                <i class="bi bi-info-circle-fill"></i>
+                <p>Aula de Surf - Introdução ao Surf</p>
+            </div>
+            <div class="price">
+                <i class="bi bi-wallet-fill"></i>
+                <p>10€</p>
+            </div>
+        </div>
+
+        <div class="final-botao">
+            <a class="btninscrever" href="#" role="button">Inscrever</a>
+        </div>
+
+    </div>
+    <div class="border rounded shadow-sm cardaula">
+
+        <div class="areadate">
+            <h2>SAB</h2>
+            <h1>24</h1>
+        </div>
+
+        <div class="vr"></div>
+
+        <div class="horaloc">
+            <div class="hora">
+                <i class="bi bi-clock-fill"></i>
+                <p>9:00 - 11:00</p>
+            </div>
+            <div class="loc">
+                <i class="bi bi-geo-alt-fill"></i>
+                <p>Praia de Carcavelos</p>
+            </div>
+        </div>
+
+        <div class="tipoprice">
+            <div class="tipo">
+                <i class="bi bi-info-circle-fill"></i>
+                <p>Aula de Surf - Introdução ao Surf</p>
+            </div>
+            <div class="price">
+                <i class="bi bi-wallet-fill"></i>
+                <p>10€</p>
+            </div>
+        </div>
+
+        <div class="final-botao">
+            <a class="btninscrever" href="#" role="button">Inscrever</a>
+        </div>
+
+    </div>
+    <div class="tltmes">
+        <h2>Junho, 2025</h2>
+    </div>
+    <div class="border rounded shadow-sm cardaula">
+
+        <div class="areadate">
+            <h2>DOM</h2>
+            <h1>22</h1>
+        </div>
+    
+        <div class="vr"></div>
+    
+        <div class="horaloc">
+            <div class="hora">
+                <i class="bi bi-clock-fill"></i>
+                <p>9:00 - 12:00</p>
+            </div>
+            <div class="loc">
+                <i class="bi bi-geo-alt-fill"></i>
+                <p>Praia do Guincho</p>
+            </div>
+        </div>
+    
+        <div class="tipoprice">
+            <div class="tipo">
+                <i class="bi bi-info-circle-fill"></i>
+                <p>Aula de Surf - Surf Avançado</p>
+            </div>
+            <div class="price">
+                <i class="bi bi-wallet-fill"></i>
+                <p>15€</p>
+            </div>
+        </div>
+    
+        <div class="final-botao">
+            <a class="btninscrever" href="#" role="button">Inscrever</a>
+        </div>
+    </div>
+    <div class="border rounded shadow-sm cardaula">
+
+        <div class="areadate">
+            <h2>SAB</h2>
+            <h1>28</h1>
+        </div>
+    
+        <div class="vr"></div>
+    
+        <div class="horaloc">
+            <div class="hora">
+                <i class="bi bi-clock-fill"></i>
+                <p>9:00 - 12:00</p>
+            </div>
+            <div class="loc">
+                <i class="bi bi-geo-alt-fill"></i>
+                <p>Praia S. Pedro de Moel</p>
+            </div>
+        </div>
+    
+        <div class="tipoprice">
+            <div class="tipo">
+                <i class="bi bi-info-circle-fill"></i>
+                <p>Aula de Surf - Surf Avançado</p>
+            </div>
+            <div class="price">
+                <i class="bi bi-wallet-fill"></i>
+                <p>15€</p>
+            </div>
+        </div>
+    
+        <div class="final-botao">
+            <a class="btninscrever" href="#" role="button">Inscrever</a>
+        </div>
+    </div>  
+@endsection
+
+@section('scripts')
+<script src="{{asset('js/login_animation.js')}}"></script>
+<script src="{{asset('js/dropdown_animation.js')}}"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+@endsection
---
+++ BeachTribe_Laravel/resources/views/layout/master.blade.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>@yield('title')</title>
+    
+    @yield('styles')
+    
+    @yield('scripts')
+
+</head>
+
+<body>
+    <nav>
+        <div id="nav_logo_div">
+            <a href="index.html">
+                <img src="{{asset('img/LightHorizontal.png')}}">
+            </a>
+        </div>
+        <div id="nav_section">
+            <div id="nav_main">
+                <div id="nav_aaa">
+                    <a class="content_nav nav_widescreen" href="https://wordpress.g6.dwm202425.online/">
+                        <p>Loja</p>
+                    </a>
+                    <a class="content_nav nav_widescreen" href="eventos.html">
+                        <p>Eventos</p>
+                    </a>
+                    <a class="content_nav nav_widescreen" href="modalidades.html">
+                        <p>Modalidades</p>
+                    </a>
+                    <a class="content_nav nav_widescreen" href="calendario.html">
+                        <p>Calendário</p>
+                    </a>
+                    <a class="content_nav nav_widescreen" href="contactos.html">
+                        <p>Contactos</p>
+                    </a>
+                    <a class="content_nav nav_widescreen" href="sobrenos.html">
+                        <p>Sobre Nós</p>
+                    </a>
+                    <a class="content_nav nav_widescreen" id="nav_login" href="login.html">
+                        <p class="nav_widescreen">Log in</p>
+                    <img id="login_logo" class="nav_widescreen" src="{{asset('img/login_logo.png')}}">
+                    </a>
+                </div>
+                <a class="dropbtn">
+                    <img id="dropdownIcon1" class="dorpdownIcons" src="{{asset('img/dropdown1.png')}}">
+                    <img id="dropdownIcon2" class="dorpdownIcons" src="{{asset('img/dropdown2.png')}}">
+                </a>
+            </div>
+            <div class="content_nav dropdown">
+                <div class="dropdown-content">
+                    <a class="nav_responsive" href="https://wordpress.g6.dwm202425.online/">
+                        <p>Loja</p>
+                    </a>
+                    <a class="nav_responsive" href="eventos.html">
+                        <p>Eventos</p>
+                    </a>
+                    <a class="nav_responsive" href="modalidades.html">
+                        <p>Modalidades</p>
+                    </a>
+                    <a class="nav_responsive" href="calendario.html">
+                        <p>Calendário</p>
+                    </a>
+                    <a class="nav_responsive" href="contactos.html">
+                        <p>Contactos</p>
+                    </a>
+                    <a class="nav_responsive" href="sobrenos.html">
+                        <p>Sobre Nós</p>
+                    </a>
+                    <a class="nav_responsive" href="login.html">
+                        <p>Log in</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div id="nav_spacer"></div>
+
+<section>
+    @yield('content')
+</section>
+
+    <footer>
+        <div id="footer_body">
+            <div id="footer_section1">
+                <a class="content_footer" id="title_footer" href="https://wordpress.g6.dwm202425.online/">
+                    <p>Loja</p>
+                </a>
+                <div id="footer_line1"></div>
+                <div id="footer_table">
+                    <div class="footer_textarea" id="footer_textarea_1">
+                        <a class="content_footer" href="https://wordpress.g6.dwm202425.online/product-category/merch/">
+                            <p>Merch</p>
+                        </a>
+                        <a class="content_footer"
+                            href="https://wordpress.g6.dwm202425.online/product-category/acessorios/">
+                            <p>Acessórios</p>
+                        </a>
+                        <a class="content_footer"
+                            href="https://wordpress.g6.dwm202425.online/product-category/chapeus/">
+                            <p>Chapéus</p>
+                        </a>
+                        <a class="content_footer" href="https://wordpress.g6.dwm202425.online/product-category/malas/">
+                            <p>Malas</p>
+                        </a>
+                    </div>
+                    <div class="footer_textarea">
+                        <a class="content_footer"
+                            href="https://wordpress.g6.dwm202425.online/product-category/material-desporto/">
+                            <p>Material Desportivo</p>
+                        </a>
+                        <a class="content_footer" href="https://wordpress.g6.dwm202425.online/product-category/praia/">
+                            <p>Praia</p>
+                        </a>
+                        <a class="content_footer" href="https://wordpress.g6.dwm202425.online/product-category/roupas/">
+                            <p>Roupa</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div id="footer_section2">
+                <div id="footer_line2"></div>
+                <div class="footer_textarea">
+                    <a class="content_footer" href="eventos.html">
+                        <p>Eventos</p>
+                    </a>
+                    <a class="content_footer" href="modalidades.html">
+                        <p>Modalidades</p>
+                    </a>
+                    <a class="content_footer" href="calendario.html">
+                        <p>Calendário</p>
+                    </a>
+                    <a class="content_footer" href="contactos.html">
+                        <p>Contactos</p>
+                    </a>
+                    <a class="content_footer" href="sobrenos.html">
+                        <p>Sobre Nós</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
