337c59635d5976def46a2e83c5af0100a664fcaf
Marceloooxx <2231340@my.ipleiria.pt>
2024-11-27 12:59:49 +0000

    Added events_table

---
+++ BeachTribe/database/migrations/2024_11_27_122314_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->enum('type', ['Formações', 'Festivais']);
+            $table->enum('state', ['ativado', 'desativado'])->default('desativado');
+            $table->string('local', 50);
+            $table->dateTime('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
+
+
