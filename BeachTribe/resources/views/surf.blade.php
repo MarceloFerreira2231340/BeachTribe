6b0961b55743c7481a787a65bdcf3a9b6492aaf0
Marceloooxx <2231340@my.ipleiria.pt>
2024-11-28 09:05:07 +0000

    Fixed Carolin's surf page

--- BeachTribe/resources/views/surf.blade.php
+++ BeachTribe/resources/views/surf.blade.php
@@ -3,7 +3,7 @@
 
 @section('styles')
 <link rel="stylesheet" href="{{asset('css/normalize.css')}}">
-        <link rel="stylesheet" href="{{asset('css/Carolina/style.css')}}">
+<link rel="stylesheet" href="{{asset('css/Carolina/style.css')}}">
         <link rel="stylesheet" href="https://use.typekit.net/oov2wcw.css">
 
 <!-- Favicon-->
@@ -16,12 +16,11 @@
 <!-- Core theme CSS (includes Bootstrap)-->
 <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
 <link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.3/components/heroes/hero-1/assets/css/hero-1.css" />
-<link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
 @endsection
 
 @section('content')
 <!-- HEROE BANNER-->
-<section class="bsb-hero-1 px-3 bsb-overlay bsb-hover-pull" style="background-image: url('{{ asset('img/Carolina/surfbanner.jpg') }}');">
+<section class="bsb-hero-1 px-3 bsb-overlay bsb-hover-pull" style="background-image: url('{{ asset('img/Carolina/surfbanner.jpg') }}')">
     <div class="container">
         <div class="row justify-content-md-center">
             <div class="col-12 col-md-11 col-lg-9 col-xl-7 col-xxl-6 text-center text-white">
@@ -53,17 +52,17 @@
     <div class="container">
         <div class="row gy-4">
             <div class="col-md-4">
-                <img src="{{('img/Carolina/surfing.png')}}" alt="Aulas de Surf">
-                <h4>Aulas de Surf</h4>
+                <img src="{{ asset('img/Carolina/surfing.png')}}" alt="Aulas de Surf">
+                <img src="{{ asset('img/Carolina/surfing.png') }}" alt="Aulas de Surf">
                 <p>Aulas de Surf para todas as idades. Aprenda a surfar connosco, divertimento garantido!</p>
             </div>
             <div class="col-md-4">
-                <img src="{{('img/Carolina/coupon.png')}}" alt="Surf Voucher">
+                <img src="{{ asset('img/Carolina/coupon.png') }}" alt="Surf Voucher">
                 <h4>Surf Voucher</h4>
                 <p>Ofereça a melhor prenda de aniversário, um surf voucher.</p>
             </div>
             <div class="col-md-4">
-                <img src="{{('img/Carolina/sun.png')}}" alt="Dia de Surf">
+                <img src="{{ asset('img/Carolina/sun.png') }}" alt="Dia de Surf">
                 <h4>Dia de Surf</h4>
                 <p>Um dia dedicado ao Surf, nas melhores praias de Portugal.</p>
             </div>
