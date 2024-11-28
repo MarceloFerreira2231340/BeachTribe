e06c454e6df8f50b15044dab09f3e5c04cffa2a5
Marceloooxx <2231340@my.ipleiria.pt>
2024-11-28 11:14:18 +0000

    Removed php folder

--- site_beachtribe/js/highlightform.js
+++
@@ -1,9 +0,0 @@
-function highlightForm() {
-    const target = document.getElementById('LandingForm');
-    
-    target.classList.add('highlightform');
-    
-    setTimeout(() => {
-        target.classList.remove('highlightform');
-    }, 1000);
-}
