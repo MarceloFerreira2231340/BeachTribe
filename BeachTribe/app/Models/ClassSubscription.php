da117ed9847ff932a925dc55cb13f62a6c9a5281
Dinis <2231338@my.ipleiria.pt>
2024-12-03 15:58:13 +0000

    Controllers

---
+++ BeachTribe/app/Models/ClassSubscription.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClassSubscription extends Model
+{
+    use HasFactory;
+}
