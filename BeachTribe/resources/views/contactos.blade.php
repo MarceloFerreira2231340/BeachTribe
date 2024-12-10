ccbba6a928141bbe5aa391f24f9dc20ef7c96e7c
Samuel200125 <2201255@my.ipleiria.pt>
2024-12-10 15:21:45 +0000

    Update contactos.blade.php
    added phone label

--- BeachTribe/resources/views/contactos.blade.php
+++ BeachTribe/resources/views/contactos.blade.php
@@ -15,17 +15,20 @@
     <div class="contact-container">
         <h1>Contacte-nos</h1>
         <p>Bem vindo à nossa página de contactos, se tiver alguma dúvida preencha os campos a baixo.</p>
-      
+
         <form>
           <label for="name">Nome:</label>
           <input type="text" id="name" name="name" required>
-      
+
           <label for="email">Email:</label>
           <input type="email" id="email" name="email" required>
-      
+
+          <label for="phone">Telefone:</label>
+          <input type="tel" id="phone" name="phone">
+
           <label for="message">Mensagem:</label>
           <textarea id="message" name="message" required></textarea>
-      
+
           <button type="submit">Submeter</button>
         </form>
       </div>
