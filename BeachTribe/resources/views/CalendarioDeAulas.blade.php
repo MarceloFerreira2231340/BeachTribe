6517c7a2b07b92583bb5953fd18e641a81800a35
JoaoBonita2430 <2223235@my.ipleiria.pt>
2024-12-16 18:45:42 +0000

    Update geral do João
    Façam favor de dar
    php artisan refresh
    php artisan db:seed

--- BeachTribe/resources/views/CalendarioDeAulas.blade.php
+++ BeachTribe/resources/views/CalendarioDeAulas.blade.php
@@ -3,8 +3,8 @@
 @section('title', 'Calendário de Aulas')
 
 @section('styles')
-    <link rel="stylesheet" href="{{asset('css/normalize.css')}}">
-    <link rel="stylesheet" href="{{asset('css/Joao/stylesjb.css?=1.0')}}">
+    <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/Joao/stylesjb.css?=1.0') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://use.typekit.net/oov2wcw.css">
@@ -15,18 +15,17 @@
         <h1><b>Calendário de Aulas</b></h1>
         <h3>Aqui podes consultar as aulas e os horários disponíveis durante o mês</h3>
     </div>
-    
+
     @foreach($classes as $class)
         <div class="fix">
             <div class="tltmes">
-                <h2>{{ \Carbon\Carbon::parse($class->created_at)->format('F, Y') }}</h2>
+                <h2>{{ date('F, Y', strtotime($class->date)) }}</h2> <!-- Exibe o mês e ano -->
             </div>
 
             <div class="border rounded shadow-sm cardaula">
-
                 <div class="areadate">
-                    <h2>{{ \Carbon\Carbon::parse($class->created_at)->format('D') }}</h2>
-                    <h1>{{ \Carbon\Carbon::parse($class->created_at)->format('d') }}</h1>
+                    <h2>{{ date('D', strtotime($class->date)) }}</h2> <!-- Exibe o dia da semana -->
+                    <h1>{{ date('d', strtotime($class->date)) }}</h1> <!-- Exibe o dia do mês -->
                 </div>
 
                 <div class="vr"></div>
@@ -34,7 +33,7 @@
                 <div class="horaloc">
                     <div class="hora">
                         <i class="bi bi-clock-fill"></i>
-                        <p>{{ \Carbon\Carbon::parse($class->created_at)->format('H:i') }} - {{ \Carbon\Carbon::parse($class->created_at)->addHours($class->duration)->format('H:i') }}</p>
+                        <p>{{ date('H:i', strtotime($class->date)) }} - {{ date('H:i', strtotime($class->date . ' + ' . $class->duration . ' hours')) }}</p>
                     </div>
                     <div class="loc">
                         <i class="bi bi-geo-alt-fill"></i>
@@ -49,7 +48,7 @@
                     </div>
                     <div class="price">
                         <i class="bi bi-wallet-fill"></i>
-                        <p>{{ $class->price ?? 'N/A' }}€</p>
+                        <p>{{ number_format($class->price, 2, ',', '.') }}€</p> <!-- Exibe o preço -->
                     </div>
                 </div>
 
@@ -62,8 +61,8 @@
 @endsection
 
 @section('scripts')
-    <script src="{{asset('js/login_animation.js')}}"></script>
-    <script src="{{asset('js/dropdown_animation.js')}}"></script>
+    <script src="{{ asset('js/login_animation.js') }}"></script>
+    <script src="{{ asset('js/dropdown_animation.js') }}"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 @endsection
