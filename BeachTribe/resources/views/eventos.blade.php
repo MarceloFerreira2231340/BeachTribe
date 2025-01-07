f49ee7bc2bc4b5e8a08f181c23a5943057131c49
MarceloFerreira2231340 <2231340@my.ipleiria.pt>
2025-01-07 16:37:57 +0000

    create on evnt

--- BeachTribe/resources/views/eventos.blade.php
+++ BeachTribe/resources/views/eventos.blade.php
@@ -2,42 +2,52 @@
 @section('title', 'Eventos')
 
 @section('styles')
-<link rel="stylesheet" href="{{('css/Marcelo/style.css')}}">
-    <link rel="stylesheet" href="{{('css/Marcelo/normalize.css?v=1.0')}}">
+    <link rel="stylesheet" href="{{ 'css/Marcelo/style.css?v=1.0' }}">
+    <link rel="stylesheet" href="{{ 'css/Marcelo/normalize.css?v=1.0' }}">
     <link href='https://fonts.googleapis.com/css?family=IBM%20Plex%20Mono' rel='stylesheet'>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://use.typekit.net/oov2wcw.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 @endsection
 
 @section('scripts')
-<script src="{{asset('js/login_animation.js')}}"></script>
-<script src="{{asset('js/dropdown_animation.js')}}"></script>
-<script src="{{asset('js/highlightform.js')}}"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/login_animation.js') }}"></script>
+    <script src="{{ asset('js/dropdown_animation.js') }}"></script>
+    <script src="{{ asset('js/highlightform.js') }}"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 @endsection
 
 @section('content')
-<div class="beachTribeEvents">
-    <img src="{{('img/Marcelo/eventosMarcelo/mick-snap-banner.png')}}" alt="HeroImage">
-    <div class="sectionEvents">
-        <div class="sectionEventsTitle">Eventos</div>
-        <div class="sectionEventsText">Aqui podes encontrar eventos que irão decorrer nas praias dos nossos
-            clubes!<br>Desde formações até concertos, há sempre algo interessante para participar.<br><br>Dirige-te
-            à praia mais próxima ou inscreve-te online!</div>
-    </div>
+    <div class="beachTribeEvents">
+        <img src="{{ 'img/Marcelo/eventosMarcelo/mick-snap-banner.png' }}" alt="HeroImage">
+        <div class="sectionEvents">
+            <div class="sectionEventsTitle">Eventos</div>
+            <div class="sectionEventsText"><span>Aqui podes encontrar eventos que irão decorrer nas praias dos nossos
+                clubes!<br>Desde formações até concertos, há sempre algo interessante para participar.<br><br>Dirige-te
+                à praia mais próxima ou inscreve-te online!</span>
+                <span>Se tens um evento que gostarias de partilhar connosco, não hesites em contactar-nos!</span>
+            </div>
+            <div class="addEventBlade">
+                <a class="btn btn-primary" href="{{ route('admin.events.create') }}">
+                    <i class="fas fa-plus"></i> Adicionar Evento
+                </a>
+            </div>
+        </div>
 
         <div class="sectionsEventsDisplay">
 
-            @foreach($events as $event)
-                @if($event->state !== 'desativado')
+
+            @foreach ($events as $event)
+                @if ($event->state !== 'desativado')
                     <div class="sectionEventsCards">
                         <img src="{{ Storage::url('events/' . $event->image) }}" alt="Banner da Carta">
                         <div class="sectionEventsCardsInfo">
-                            <div class="sectionEventsCardsTitle">{{ $event->type}} -<br>{{ $event->title}}</div>
+                            <div class="sectionEventsCardsTitle">{{ $event->type }} -<br>{{ $event->title }}</div>
                             <div class="sectionEventsCardsDescricao">{{ $event->description }}</div>
                             <a href="#" class="sectionEventsCardsSaberMais"><span>Saber Mais</span></a>
                         </div>
