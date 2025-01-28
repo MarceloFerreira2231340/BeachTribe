ff4792e988369b13cad8d0dca0c96cb040d86789
Dinis <2231338@my.ipleiria.pt>
2025-01-28 15:11:27 +0000

    Dashboard Top of the Line!

--- BeachTribe/resources/views/_admin/dashboard.blade.php
+++ BeachTribe/resources/views/_admin/dashboard.blade.php
@@ -17,15 +17,16 @@
 					<h1 style="display:inline;"> {{ $count_classes }} </h1>
 					<h3 style="display:inline;"> Aulas </h3>
 				</div>
-
+				<a class="btn btn-primary" href="{{ route('admin.classes.create') }}"><i class="fas fa-plus"></i> Adicionar Aula</a>
 			</div>
 		</div>
-		<div class="col-4">
+		<div class="col-4" style="margin-bottom:25px;">
 			<div class="card shadow">
 				<div class="card-header p-4">
 					<h1 style="display:inline;"> {{ $count_sports }} </h1>
-					<h3 style="display:inline;"> Atividades </h3>
+					<h3 style="display:inline;"> Modalidades </h3>
 				</div>
+				<a class="btn btn-primary" href="{{ route('admin.sports.create') }}"><i class="fas fa-plus"></i> Adicionar Modalidade</a>
 			</div>
 		</div>
 		<div class="col-4">
@@ -37,6 +38,7 @@
 				<a class="btn btn-primary" href="{{ route('admin.events.create') }}"><i class="fas fa-plus"></i> Adicionar Evento</a>
 			</div>
 		</div>
+		<!--
 		<div class="col-4">
 			<div class="card shadow">
 				<div class="card-header p-4">
@@ -45,6 +47,7 @@
 				</div>
 			</div>
 		</div>
+		-->
 		<div class="col-4">
 			<div class="card shadow">
 				<div class="card-header p-4">
